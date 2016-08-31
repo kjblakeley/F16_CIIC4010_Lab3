3038117f43478accef2ec4a220ae1ead9c448f5d
4015a2 <4015a2@coco.ece.uprm.edu>
2016-08-31 09:21:09 -0400

    Question #7 Lab#3

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -7,5 +7,9 @@ public class Lab3Exercises {
 		myFrame.setLocation(850, 400);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+
+		MyPanelClass myPanel = new MyPanelClass();
+		myFrame.getContentPane().add(myPanel);
 	}
+
 }
